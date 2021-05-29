b9c74fc64dc04ae5782d480e11b2ddce851c8ecb
= <=>
2021-05-28 22:08:44 -0300

    feat: add ready log client
    Logged cliente as user.tag

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 const prefix = "!";
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on("message", function(message) {
 
