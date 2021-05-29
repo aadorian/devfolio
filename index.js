8512c29cb622e7c8479673d5b51cbf91943cac19
= <=>
2021-05-29 04:51:36 -0300

    add signup

--- index.js
+++ index.js
@@ -202,15 +202,11 @@ client.on("message", async function (message) {
     console.log(keyring.pairs.length, "pairs available");
     console.log(pair.meta.name, "has address", pair.address);
   }
-  if (command === "sign") {
-    // create the message, actual signature and verify
-    const message = stringToU8a("this is our message");
-    const signature = alice.sign(message);
-    const isValid = alice.verify(message, signature);
+  if (command === "testing") {
+  
 
-    // output the result
-    console.log(`${u8aToHex(signature)} is ${isValid ? "valid" : "invalid"}`);
-  }
+
+   }
   if (command === "multibalance") {
     const Alice = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
     //secret Alice 0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a
